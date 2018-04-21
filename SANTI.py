4b08b323be3e47f0267775a1015cfa21cfb82447
Nainggolan <Nainggolan@github.com>
2018-04-21 16:29:49 +0800

    O__O "…

---
+++ SANTI.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+SANTI = [[1, 1, 0, 1, 1, 0, 0],
+         [1, 0, 1, 1, 0, 0, 1],
+         [1, 1, 1, 1, 1, 1, 1],
+         [0, 1, 0, 1, 0, 1, 1],
+         [1, 0, 1, 1, 1, 1, 0],
+         [1, 1, 0, 1, 1, 0, 1]]
+
+santi = np.array(SANTI)
+
+print(santi)
+
+print(santi[0][0])
+
+if(santi[i][j] != 0):
+    santi[i][j+1]
+
+
+
+
+
+
+
+
+
